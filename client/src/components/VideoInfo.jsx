06a972335d5b262f5b5c78341ddf0b1546665938
ebrahimabdulnaeem <101273814+ebrahimabdulnaeem@users.noreply.github.com>
2025-02-04 00:45:19 +0200

    Fix: Add null checks and default values in VideoInfo component

--- client/src/components/VideoInfo.jsx
+++ client/src/components/VideoInfo.jsx
@@ -110,9 +110,13 @@ const VideoDescription = styled(Typography)({
 });
 
 const VideoInfo = ({ videoInfo }) => {
-  const { title, thumbnail, duration, author, views, likes, description } = videoInfo;
+  if (!videoInfo) return null;
+
+  const { title = '', thumbnail = '', duration = '', author = '', views = 0, likes = 0, description = '' } = videoInfo;
 
   const separateEmojiFromText = (text) => {
+    if (!text) return '';
+    
     const emojiRegex = /[\u{1F300}-\u{1F9FF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F900}-\u{1F9FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{1F191}-\u{1F251}]|[\u{1F004}]|[\u{1F0CF}]|[\u{1F170}-\u{1F171}]|[\u{1F17E}-\u{1F17F}]|[\u{1F18E}]|[\u{3030}]|[\u{2B50}]|[\u{2B55}]|[\u{2934}-\u{2935}]|[\u{2B05}-\u{2B07}]|[\u{2B1B}-\u{2B1C}]|[\u{3297}]|[\u{3299}]|[\u{303D}]|[\u{00A9}]|[\u{00AE}]|[\u{2122}]/gu;
     
     const parts = text.split(emojiRegex);
